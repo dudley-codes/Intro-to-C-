5a69e1928908a8fd0dda09be7300ededa561646f
dudley-codes <alexdudley@me.com>
2021-05-21 13:27:27 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.Write("First Name > ");
+string firstName = Console.ReadLine();
+
+Console.Write("Last Name > ");
+string lastName = Console.ReadLine();
+
+string reverse = string.Empty;
+
+string fullName = firstName + " " + lastName;
+
+for (int num = fullName.Length - 1; num >= 0; num--)
+{
+    reverse = reverse + fullName[num];
+}
+
+Console.WriteLine(reverse);
